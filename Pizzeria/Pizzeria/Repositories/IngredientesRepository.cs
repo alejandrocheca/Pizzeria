59d21cc39ee98ea42822510a874275adc973f4fd
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-09-22 16:43:25 +0200

    Cambios IngredientesRepository
    Cambios en archivo IngredientesRepository de la carpeta repositories

--- Pizzeria/Pizzeria/Repositories/IngredientesRepository.cs
+++ Pizzeria/Pizzeria/Repositories/IngredientesRepository.cs
@@ -12,46 +12,46 @@ namespace Pizzeria.Repositories
     {
         private readonly AppDbContext _context;
 
-        public IngredientsRepository(AppDbContext context)
+        public IngredientesRepository(AppDbContext context)
         {
             _context = context;
         }
 
-        public IEnumerable<Ingredients> Ingredients => _context.Ingredients.Include(x => x.PizzaIngredients); //include here
+        public IEnumerable<Ingredientes> Ingredientes => _context.Ingredientes.Include(x => x.PizzaIngredients); //include here
 
-        public void Add(Ingredients ingredient)
+        public void Add(Ingredientes ingrediente)
         {
-            _context.Ingredients.Add(ingredient);
+            _context.Ingredientes.Add(ingrediente);
         }
 
-        public IEnumerable<Ingredients> GetAll()
+        public IEnumerable<Ingredientes> GetAll()
         {
-            return _context.Ingredients.ToList();
+            return _context.Ingredientes.ToList();
         }
 
-        public async Task<IEnumerable<Ingredients>> GetAllAsync()
+        public async Task<IEnumerable<Ingredientes>> GetAllAsync()
         {
-            return await _context.Ingredients.ToListAsync();
+            return await _context.Ingredientes.ToListAsync();
         }
 
-        public Ingredients GetById(int? id)
+        public Ingredientes GetById(int? id)
         {
-            return _context.Ingredients.FirstOrDefault(p => p.Id == id);
+            return _context.Ingredientes.FirstOrDefault(p => p.Id == id);
         }
 
-        public async Task<Ingredients> GetByIdAsync(int? id)
+        public async Task<Ingredientes> GetByIdAsync(int? id)
         {
-            return await _context.Ingredients.FirstOrDefaultAsync(p => p.Id == id);
+            return await _context.Ingredientes.FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public bool Exists(int id)
         {
-            return _context.Ingredients.Any(p => p.Id == id);
+            return _context.Ingredientes.Any(p => p.Id == id);
         }
 
-        public void Remove(Ingredients ingredient)
+        public void Remove(Ingredientes ingrediente)
         {
-            _context.Ingredients.Remove(ingredient);
+            _context.Ingredientes.Remove(ingrediente);
         }
 
         public void SaveChanges()
@@ -64,9 +64,9 @@ namespace Pizzeria.Repositories
             await _context.SaveChangesAsync();
         }
 
-        public void Update(Ingredients ingredient)
+        public void Update(Ingredientes ingrediente)
         {
-            _context.Ingredients.Update(ingredient);
+            _context.Ingredientes.Update(ingrediente);
         }
     }
 }
