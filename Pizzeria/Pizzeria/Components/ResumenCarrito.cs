290803eb2a2638747caf9721dcf2f4df3670ce7e
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-09-25 15:24:07 +0200

    Actualizacion ResumenCarrito.cs
    Actualizacion archivo ResumenCarrito.cs

---
+++ Pizzeria/Pizzeria/Components/ResumenCarrito.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Pizzeria.Models;
+using Pizzeria.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Components
+{
+    public class ResumenCarrito : ViewComponent
+    {
+        private readonly Carrito _carrito;
+        public ResumenCarrito(Carrito carrito)
+        {
+            _carrito = carrito;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _carrito.GetShoppingCartItemsAsync();
+            _carrito.ShoppingCartItems = items;
+
+            var carritoCartViewModel = new CarritoViewModel
+            {
+                Carrito = _carrito,
+                ShoppingCartTotal = _carrito.GetShoppingCartTotal()
+            };
+            return View(carritoCartViewModel);
+        }
+    }
+}
